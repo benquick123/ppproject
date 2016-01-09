3a8c143962aa0c6a1398ba49469c611d43f6e364
Benjamin <benquick.fele@gmail.com>
2016-01-09 01:42:12 +0100

    added documentation

---
+++ docu/Miselni vzorec.mm
@@ -0,0 +1,288 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" CREATED="1452203519116" ID="ID_1537040116" MODIFIED="1452207752647" STYLE="fork" TEXT="Treniranje in ocenjevanje spomina">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Segoe UI" SIZE="14"/>
+<node CREATED="1452203638239" HGAP="-24" ID="ID_133753833" MODIFIED="1452207717806" POSITION="right" STYLE="fork" VSHIFT="-32">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 8">
+      namen
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="8"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204641855" HGAP="30" ID="ID_85531713" MODIFIED="1452207608747" STYLE="fork" VSHIFT="-11">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      za vse generacije
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="4"/>
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204651877" HGAP="35" ID="ID_1086265702" MODIFIED="1452207622093" STYLE="fork" VSHIFT="18">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      aktivno pre&#382;ivljanje prostega &#269;asa
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452205280359" HGAP="37" ID="ID_582777162" MODIFIED="1452207633844" STYLE="fork" VSHIFT="14">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      izbolj&#353;anje spomina
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452205292382" ID="ID_55569681" MODIFIED="1452207256634" STYLE="fork" TEXT="kratkoro&#x10d;nega">
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452205299062" ID="ID_185753273" MODIFIED="1452207256634" STYLE="fork" TEXT="delovnega">
+<edge COLOR="#3366ff" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452205986957" ID="ID_1325549473" MODIFIED="1452207256634" STYLE="fork" TEXT="prostorskega">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1452203652640" HGAP="-7" ID="ID_641483559" MODIFIED="1452207721839" POSITION="left" STYLE="fork" VSHIFT="-94">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 8">
+      mo&#382;ne te&#382;ave
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#ff6633" STYLE="bezier" WIDTH="8"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204253924" HGAP="30" ID="ID_157366742" MODIFIED="1452207596491" STYLE="fork" VSHIFT="-22">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      zamuda roka za oddajo
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#ff6633" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204264699" ID="ID_1786904502" MODIFIED="1452207590813" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      (pre)malo testiranja
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#ff6633" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204280292" HGAP="32" ID="ID_1361333641" MODIFIED="1452207256634" STYLE="fork" TEXT="previsoka/prenizka zahtevnost" VSHIFT="-33">
+<edge COLOR="#ff6633" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204311266" HGAP="60" ID="ID_1484096338" MODIFIED="1452207256634" STYLE="fork" TEXT="hro&#x161;&#x10d;i" VSHIFT="12">
+<edge COLOR="#ff6633" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1452203694886" HGAP="-44" ID="ID_342014423" MODIFIED="1452207713038" POSITION="right" STYLE="fork" VSHIFT="98">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 8">
+      funkcionalnosti
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#009900" STYLE="bezier" WIDTH="8"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452203782501" HGAP="42" ID="ID_1489469953" MODIFIED="1452207646515" STYLE="fork" VSHIFT="-23">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      pomnenje
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#009900" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452203793886" HGAP="42" ID="ID_899166559" MODIFIED="1452207256634" STYLE="fork" TEXT="&#x161;tevil" VSHIFT="2">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452203800022" HGAP="43" ID="ID_1675825242" MODIFIED="1452207256633" STYLE="fork" TEXT="likov" VSHIFT="1">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452203851709" HGAP="44" ID="ID_529710224" MODIFIED="1452207256633" STYLE="fork" TEXT="lokacije" VSHIFT="-19">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1452203917836" HGAP="73" ID="ID_415217063" MODIFIED="1452207654525" STYLE="fork" VSHIFT="-9">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      na&#269;ini
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#009900" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452203924876" ID="ID_1203837698" MODIFIED="1452207256633" STYLE="fork" TEXT="igralni">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452203933823" ID="ID_577520411" MODIFIED="1452207256633" STYLE="fork" TEXT="vadbeni">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1452203995280" HGAP="78" ID="ID_272274415" MODIFIED="1452280227897" STYLE="fork" VSHIFT="20">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      zahtevnost
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#009900" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204191253" HGAP="45" ID="ID_1639057597" MODIFIED="1452207256633" STYLE="fork" TEXT="samodejno pove&#x10d;evanje" VSHIFT="-13">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204202796" HGAP="37" ID="ID_643144754" MODIFIED="1452207256633" STYLE="fork" TEXT="ro&#x10d;na prednastavitev" VSHIFT="13">
+<edge COLOR="#009900" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1452204153605" HGAP="71" ID="ID_379898869" MODIFIED="1452207669099" STYLE="fork" VSHIFT="26">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      lestvica najbolj&#353;ih rezultatov
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#009900" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452280231006" HGAP="72" ID="ID_1129248007" MODIFIED="1452280317389" VSHIFT="6">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      elaboriranje rezultatov
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge WIDTH="4"/>
+</node>
+</node>
+<node CREATED="1452203714358" HGAP="2" ID="ID_856756995" MODIFIED="1452207724998" POSITION="left" STYLE="fork" VSHIFT="162">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 8">
+      uporabni&#353;ki vmesnik
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#cccc00" STYLE="bezier" WIDTH="8"/>
+<font NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204368482" HGAP="63" ID="ID_287154859" MODIFIED="1452207534836" STYLE="fork" VSHIFT="-3">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      predvsem preprost
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#cccc00" STYLE="bezier" WIDTH="4"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204468744" HGAP="38" ID="ID_1958959524" MODIFIED="1452207516109" STYLE="fork" VSHIFT="27">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 4">
+      minimalisti&#269;en dizajn
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#cccc00" STYLE="bezier" WIDTH="4"/>
+<font BOLD="true" NAME="Segoe UI" SIZE="12"/>
+<node CREATED="1452204481384" HGAP="21" ID="ID_1603139839" MODIFIED="1452207256633" STYLE="fork" TEXT="&quot;&#x10d;ist&quot; uporabni&#x161;ki vmesnik" VSHIFT="-27">
+<edge COLOR="#cccc00" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+<node CREATED="1452204492000" HGAP="22" ID="ID_1242276167" MODIFIED="1452207256633" STYLE="fork" TEXT="veliki gumbi" VSHIFT="27">
+<edge COLOR="#cccc00" STYLE="bezier" WIDTH="2"/>
+<font NAME="Segoe UI" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
